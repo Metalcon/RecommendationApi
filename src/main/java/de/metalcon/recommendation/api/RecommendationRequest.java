ca6a9f1dbe27cefc73dc011fbef71966fac221fe
Rene Pickhardt <rene@rene-pickhardt.de>
2014-04-19 10:52:16 +0200

    also added a class for adding recommendations to the server. this cannot be done in bulk yet

--- src/main/java/de/metalcon/recommendation/api/RecommendationRequest.java
+++ src/main/java/de/metalcon/recommendation/api/RecommendationRequest.java
@@ -2,7 +2,8 @@ package de.metalcon.recommendation.api;
 
 import de.metalcon.api.requests.Request;
 
-
 public class RecommendationRequest extends Request {
+
+    private static final long serialVersionUID = 2185425171462876492L;
 
 }
